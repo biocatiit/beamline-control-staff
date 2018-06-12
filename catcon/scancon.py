a9c48b78b3c63b8d9143c6404251a947cd8f3c3f
Jesse Hopkins <hopkins.jesse@gmail.com>
2018-06-12 11:52:45 -0500

    Added a new file which will have a scan control in it.

---
+++ catcon/scancon.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# coding: utf-8
+#
+#    Project: BioCAT staff beamline control software (CATCON)
+#             https://github.com/biocatiit/beamline-control-staff
+#
+#
+#    Principal author:       Jesse Hopkins
+#
+#    This is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This software is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+from builtins import object, range, map
+from io import open
+
+import os
+
+import wx
+import wx.lib.buttons as buttons
+
+import utils
+utils.set_mppath() #This must be done before importing any Mp Modules.
+import Mp as mp
+import MpCa as mpca
+import MpWx as mpwx
+import MpWxCa as mpwxca
+
+
