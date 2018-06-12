4c2918ca7e770e54d2fb46222ee89ad993f3caa0
Jesse Hopkins <hopkins.jesse@gmail.com>
2018-06-12 17:12:57 -0500

    Added nominally working scan process to scancon.py. Need GUI wrapper to be able to test it.

--- catcon/scancon.py
+++ catcon/scancon.py
@@ -24,10 +24,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from builtins import object, range, map
 from io import open
 
+import multiprocessing
+import queue
+import tempfile
 import os
+import math
 
 import wx
-import wx.lib.buttons as buttons
 
 import utils
 utils.set_mppath() #This must be done before importing any Mp Modules.
@@ -37,3 +40,168 @@ import MpWx as mpwx
 import MpWxCa as mpwxca
 
 
+class Scanner(multiprocessing.Process):
+
+    def __init__(self, command_queue, return_queue, abort_event):
+        multiprocessing.Process.__init__(self)
+        self.daemon = True
+
+        self.command_queue = command_queue
+        self.return_queue = return_queue
+        self._abort_event = abort_event
+        self._stop_event = multiprocessing.Event()
+
+        self._commands = {'start_mxdb'      : self._start_mxdb,
+                        'set_scan_params'   : self._set_devices,
+                        'scan'              : self._scan,
+                        }
+
+    def run(self):
+        while True:
+            try:
+                cmd, args, kwargs = self.command_queue.get_nowait()
+                print(cmd)
+            except queue.Empty:
+                cmd = None
+
+            if self._abort_event.is_set():
+                self._abort()
+                cmd = None
+
+            if self._stop_event.is_set():
+                self._abort()
+                break
+
+            if cmd is not None:
+                try:
+                    self.working=True
+                    self._commands[cmd](*args, **kwargs)
+                    self.working=False
+                except Exception as e:
+                    self.working=False
+                    print('What was that? Sorry, I could not run that command.')
+                    print(e)
+
+        if self._stop_event.is_set():
+            self._stop_event.clear()
+        else:
+            self._abort()
+
+    def _start_mxdb(self, db_path):
+        self.db_path = db_path
+        print("MX Database : %s is being downloaded..."%(self.db_path))
+        self.mx_database = mp.setup_database(self.db_path)
+        self.mx_database.set_plot_enable(2)
+        print("Database has been set up")
+
+    def _set_scan_params(self, device, start, stop, step, scalers,
+        dwell_time, timer, detector=None, file_name=None, dir_path=None):
+        self.out_path = dir_path
+        self.out_name = file_name
+
+        self.device = device
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+        self.scalers = scalers
+        self.dwell_time = dwell_time
+        self.timer = timer
+        self.detector = detector
+
+    def _scan(self):
+        """
+        scan a record
+        """
+        all_names = [r.name for r in self.mx_database.get_all_records()]
+
+        if self.out_name is not None:
+            scan_name = self.out_name
+        else:
+            scan_name = self.device
+            i=1
+            while scan_name in all_names:
+                if i == 1:
+                    scan_name = "{}_{}".format(scan_name, str(i).zfill(2))
+                else:
+                    scan_name = "{}_{}".format(scan_name[:-2], str(i).zfill(2))
+                i=i+1
+
+        print("Scanning %s" % (scan_name))
+
+        description = ('{} scan linear_scan motor_scan "" "" '.format(scan_name))
+
+        num_scans = 1
+        num_motors = 1
+        num_independent_variables = num_motors
+
+        description = description + ("{} {} {} {} ".format(num_scans,
+            num_independent_variables, num_motors, str(self.device)))
+
+        scalers_detector = list(self.scalers)
+
+        if self.detector is not None:
+            scalers_detector.append(self.detector['name'])
+
+        description = description + ("{} ".format(len(scalers_detector)))
+
+        for j in range(len(scalers_detector)):
+            description = description + ("{} ".format(scalers_detector[j]))
+
+        scan_flags = 0x0
+        settling_time = 0.0
+        measurement_type = "preset_time"
+        measurement_time = self.dwell_time
+
+        description = description + (
+                '%x %f %s "%f %s" ' % (scan_flags, settling_time, measurement_type, measurement_time, self.timer))
+
+        if self.out_path is None:
+            standard_paths = wx.StandardPaths.Get()
+            tmpdir = standard_paths.GetTempDir()
+            fname = tempfile.NamedTemporaryFile(dir=tmpdir).name
+        else:
+            fname = os.path.join(self.out_path, self.out_name)
+
+        datafile_description = "sff"
+        datafile_name = fname
+        plot_description = "none"
+        plot_arguments = "$f[0]"
+
+        description = description + (
+                "%s %s %s %s " % (datafile_description, datafile_name, plot_description, plot_arguments))
+
+        description = description + ("{} {} ".format(self.start, self.step))
+
+        num_measurements = int(abs(math.floor((self.stop - self.start)/self.step)))
+        description = description + ("{} ".format(num_measurements))
+
+        print("Description = %s" % (description))
+
+        self.mx_database.create_record_from_description(description)
+
+        scan = self.mx_database.get_record(scan_name)
+
+        scan.finish_record_initialization()
+
+        self.return_queue.put_nowait([datafile_name])
+
+        scan.perform_scan()
+
+        print("%s has been performed" % (scan_name))
+
+        self.return_queue.put_nowait(['stop_live_plotting'])
+
+    def _abort(self):
+        """Clears the ``command_queue`` and aborts all current actions."""
+        while True:
+            try:
+                self.command_queue.get_nowait()
+            except queue.Empty:
+                break
+
+        self._abort_event.clear()
+
+    def stop(self):
+        """Stops the thread cleanly."""
+        self._stop_event.set()
