690ff93c368b72ef198a35a625390b3140c751c7
jbhopkins <hopkins.jesse@gmail.com>
2024-10-14 12:07:07 -0500

    Added E1608 support for measurement computing launcher.

--- epics_screens/medm_start_scripts/start_meascomp_screen.sh
+++ epics_screens/medm_start_scripts/start_meascomp_screen.sh
@@ -14,4 +14,6 @@ fi
 
 if [[ "$PREFIX" == "ETC" ]]; then
 medm -x -noMsg -macro "P=18ID:ETC:,Ti=Ti,Ct=Counter,Bo=Bo,Bi=Bi,Bd=Bd,Li=Li,Lo=Lo" ETC_module.adl &
+elif [[ "$PREFIX" == "E1608" ]]; then
+    medm -x -noMsg -macro "P=18ID:E1608:,Ai=Ai,Ao=Ao,Ct=Counter,Bo=Bo,Bi=Bi,Bd=Bd,Li=Li,Lo=Lo,Tg=Trig,Wd=WaveDig" E1608_module.adl &
 fi
