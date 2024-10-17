7d4baef8246287b68ed40f90741b8651e0f9ac64
jbhopkins <hopkins.jesse@gmail.com>
2024-10-17 10:08:31 -0500

    Updated Galil displays. Updated slit screen start script for soft motor style slis.

--- epics_screens/medm_start_scripts/start_slit_screen.sh
+++ epics_screens/medm_start_scripts/start_slit_screen.sh
@@ -13,15 +13,15 @@ else
 fi
 
 if [[ "$RECPREFIX" == "XenocsUS" ]]; then
-    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsUS,V=XenocsUS_V,H=XenocsUS_H" 4slitGraphic.adl &
+    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsUS,V=XenocsUS_V:,H=XenocsUS_H:" 4slitGraphic_soft.adl &
 elif [[ "$RECPREFIX" == "XenocsDS" ]]; then
-    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsDS,V=XenocsDS_V,H=XenocsDS_H" 4slitGraphic.adl &
+    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsDS,V=XenocsDS_V:,H=XenocsDS_H:" 4slitGraphic_soft.adl &
 elif [[ "$RECPREFIX" == "XenocsUS_V" ]]; then
-    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsUS_V,mXp=25,mXn=26" 2slit.adl &
+    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsUS_V:,mXp=25,mXn=26" 2slit_soft.adl &
 elif [[ "$RECPREFIX" == "XenocsUS_H" ]]; then
-    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsUS_H,mXp=28,mXn=27" 2slit.adl &
+    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsUS_H:,mXp=28,mXn=27" 2slit_soft.adl &
 elif [[ "$RECPREFIX" == "XenocsDS_V" ]]; then
-    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsDS_V,mXp=29,mXn=30" 2slit.adl &
+    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsDS_V:,mXp=29,mXn=30" 2slit_soft.adl &
 elif [[ "$RECPREFIX" == "XenocsDS_H" ]]; then
-    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsDS_H,mXp=32,mXn=31" 2slit.adl &
+    medm -x -macro "P=18ID_DMC_E04:,SLIT=XenocsDS_H:,mXp=32,mXn=31" 2slit_soft.adl &
 fi
