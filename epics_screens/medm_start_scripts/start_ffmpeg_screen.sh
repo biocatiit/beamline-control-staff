9fe301a97b0a1cd9896bbcdf747dc1deb1a25262
jbhopkins <hopkins.jesse@gmail.com>
2025-08-15 11:14:29 -0500

    Added other FLIR cameras to medm start scripts.

--- epics_screens/medm_start_scripts/start_ffmpeg_screen.sh
+++ epics_screens/medm_start_scripts/start_ffmpeg_screen.sh
@@ -2,4 +2,23 @@
 
 source source_medm.sh
 
+if [ -z "$1" ]
+then
+      # No args provided, default
+      export CAMERACHOICE=inline
+else
+      # Record prefix provided as argument 1
+      export CAMERACHOICE=$1
+fi
+
+if [[ "$CAMERACHOICE" == "tripod" ]]; then
 medm -x -macro "P=18ID:FLIR:D:Tripod:, R=ffmstream1:" ffmpegStream.adl &
+elif [[ "$CAMERACHOICE" == "inline" ]]; then
+medm -x -macro "P=18ID:FLIR:D:Inline:, R=ffmstream1:" ffmpegStream.adl &
+elif [[ "$CAMERACHOICE" == "screen" ]]; then
+medm -x -macro "P=18ID:FLIR:C:Screen:, R=ffmstream1:" ffmpegStream.adl &
+elif [[ "$CAMERACHOICE" == "mono1" ]]; then
+medm -x -macro "P=18ID:FLIR:C:Mono1:, R=ffmstream1:" ffmpegStream.adl &
+elif [[ "$CAMERACHOICE" == "mono2" ]]; then
+medm -x -macro "P=18ID:FLIR:C:Mono2:, R=ffmstream1:" ffmpegStream.adl &
+fi
--- epics_screens/medm_start_scripts/start_flir_screen.sh
+++ epics_screens/medm_start_scripts/start_flir_screen.sh
@@ -2,4 +2,24 @@
 
 source source_medm.sh
 
+#Check provided arguments for record prefix
+if [ -z "$1" ]
+then
+      # No args provided, default
+      export CAMERACHOICE=inline
+else
+      # Record prefix provided as argument 1
+      export CAMERACHOICE=$1
+fi
+
+if [[ "$CAMERACHOICE" == "tripod" ]]; then
 medm -x -macro "P=18ID:FLIR:D:Tripod:, R=cam1:, C=FLIR-BFS-PGE-23S6C" ADSpinnaker.adl &
+elif [[ "$CAMERACHOICE" == "inline" ]]; then
+medm -x -macro "P=18ID:FLIR:D:Inline:, R=cam1:, C=FLIR-BFS-PGE-23S6C" ADSpinnaker.adl &
+elif [[ "$CAMERACHOICE" == "screen" ]]; then
+medm -x -macro "P=18ID:FLIR:C:Screen:, R=cam1:, C=FLIR-BFS-PGE-23S6M" ADSpinnaker.adl &
+elif [[ "$CAMERACHOICE" == "mono1" ]]; then
+medm -x -macro "P=18ID:FLIR:C:Mono1:, R=cam1:, C=FLIR-BFS-PGE-23S6M" ADSpinnaker.adl &
+elif [[ "$CAMERACHOICE" == "mono2" ]]; then
+medm -x -macro "P=18ID:FLIR:C:Mono2:, R=cam1:, C=FLIR-BFS-PGE-23S6M" ADSpinnaker.adl &
+fi
