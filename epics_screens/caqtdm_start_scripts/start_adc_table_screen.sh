9205ff013a01b54785f4f348797b67358f1d7a36
jbhopkins <hopkins.jesse@gmail.com>
2026-09-02 16:22:40 -0500

    Fixed EPICS Optics caqtdm screen for 3 point table with PNC geometry.

--- epics_screens/caqtdm_start_scripts/start_adc_table_screen.sh
+++ epics_screens/caqtdm_start_scripts/start_adc_table_screen.sh
@@ -2,4 +2,4 @@
 
 source source_caqtdm.sh
 
-caQtDM -attach -noMsg -macro "P=,Q=18ID:ADCTable,T=18ID:ADCTable,M0X=18ID_DMC_E05:36,M0Y=18ID_DMC_E05:34,M1Y=18ID_DMC_E05:37,M2X=18ID_DMC_E05:35,M2Y=18ID_DMC_E05:36,M2Z=m6" table.adl &
+caQtDM -attach -noMsg -macro "P=,Q=18ID:ADCTable,T=18ID:ADCTable,M0X=18ID_DMC_E05:36,M0Y=18ID_DMC_E05:34,M1Y=18ID_DMC_E05:37,M2X=18ID_DMC_E05:35,M2Y=18ID_DMC_E05:33,M2Z=m6" table_soft.ui &
